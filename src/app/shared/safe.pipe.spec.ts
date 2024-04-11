f263b1615c843b596f8795843e454a3189e665f7
Kiril <kirilbuga@yahoo.com>
2024-04-11 18:12:22 +0200

    Add TestBed to SafePipe unit test
    In the SafePipe unit test, configured TestBed and injected DomSanitizer to create SafePipe instance. The purpose of this update is to facilitate the manipulation and control of testing environments, ensuring the tests run in a more realistic and effective way.

--- src/app/shared/safe.pipe.spec.ts
+++ src/app/shared/safe.pipe.spec.ts
@@ -1,8 +1,16 @@
+import { TestBed } from '@angular/core/testing';
 import { SafePipe } from './safe.pipe';
+import { DomSanitizer } from '@angular/platform-browser';
 
 describe('SafePipe', () => {
+  let pipe: SafePipe;
+
+  beforeEach(() => {
+    const domSanitizer = TestBed.inject(DomSanitizer);
+    pipe = new SafePipe(domSanitizer);
+  });
+
   it('create an instance', () => {
-    const pipe = new SafePipe();
     expect(pipe).toBeTruthy();
   });
 });
