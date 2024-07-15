131eea06afb05a951d4ceb56144f83f5a97c35a4
Kiril <kirilbuga@yahoo.com>
2024-07-15 21:39:11 +0200

    Update message UI with reactive forms

--- src/app/features/game-designer/tab-contexts/game-context-message/game-context-message.component.ts
+++ src/app/features/game-designer/tab-contexts/game-context-message/game-context-message.component.ts
@@ -1,46 +1,90 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
-import {Message} from "../../../../core/models/game";
-import {FormControl, FormGroup} from "@angular/forms";
+    import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+    import {Message} from "../../../../core/models/game";
+    import {AbstractControl, Form, FormArray, FormControl, FormGroup} from "@angular/forms";
 
-@Component({
-    selector: 'app-game-context-message',
-    templateUrl: './game-context-message.component.html',
-    styleUrl: './game-context-message.component.css'
-})
-export class GameContextMessageComponent implements OnInit {
-    @Input() messages!: Message[];
-    @Output() messageChange = new EventEmitter<Message>();
-    //@Input() contextForm!: FormGroup;
-    messageForm: FormGroup = new FormGroup({
-        botMessage: new FormControl("", ),
-        userMessage: new FormControl("", ),
-    });
+    @Component({
+        selector: 'app-game-context-message',
+        templateUrl: './game-context-message.component.html',
+        styleUrl: './game-context-message.component.css'
+    })
+    export class GameContextMessageComponent implements OnInit {
+        @Input() messages!: Message[];
+        @Output() messageChange = new EventEmitter<Message>();
+        //@Input() contextForm!: FormGroup;
+        messageForm: FormGroup = new FormGroup({
+            botMessage: new FormControl("",),
+            userMessage: new FormControl("",),
+        });
 
+        updateForm: FormGroup = new FormGroup({
+            messages: new FormArray([])
+        })
 
-    constructor() {
-    }
+        isEditing: boolean = false;
 
-    ngOnInit(): void {
-    }
+        constructor() {
+        }
 
-    get botMessage() { return this.messageForm.get('botMessage'); }
-    get userMessage() { return this.messageForm.get('userMessage'); }
+        ngOnInit(): void {
+        }
 
-    addMessage() {
-        const newMessage: Message = {
-            intent: this.messages.length,
-            response: this.userMessage?.value,
-            utterance: this.botMessage?.value,
-        };
+        get botMessage() {
+            return this.messageForm.get('botMessage');
+        }
+        get userMessage() {
+            return this.messageForm.get('userMessage');
+        }
+        get messagesFormArray() {
+            return this.updateForm.get('messages') as FormArray;
+        }
 
-        this.messageForm.reset({
-                botMessage: "",
-                userMessage: "",
+        addMessage() {
+            const newMessage: Message = {
+                intent: this.messages.length,
+                response: this.botMessage?.value,
+                utterance: this.userMessage?.value,
+            };
+
+            this.messageForm.reset({
+                    botMessage: "",
+                    userMessage: "",
+                }
+            );
+            console.log("newMessage: ", newMessage);
+            //addMessage(newMessage);
+            this.messageChange.emit(newMessage);
+        }
+
+        updateMessage(index: number) {
+            // Log the form group values for debugging
+            const messageFormGroup = this.messagesFormArray.at(index) as FormGroup;
+            const updatedMessage: Message = {
+                intent: -1,
+                response: messageFormGroup.get('botMessage')?.value,
+                utterance: messageFormGroup.get('userMessage')?.value,
+            };
+            this.messageChange.emit(updatedMessage);
+            console.log("updateMessage: ", updatedMessage);
+            //this.toggleEdit();
+        }
+
+        toggleEdit() {
+            this.isEditing = !this.isEditing;
+            if (this.isEditing) {
+                this.populateFormArray();
             }
-        );
-        console.log("newMessage: ", newMessage);
-        //addMessage(newMessage);
-        this.messageChange.emit(newMessage);
-    }
+        }
 
-}
+        populateFormArray() {
+            const messagesArray = this.updateForm.get('messages') as FormArray;
+            messagesArray.clear();
+            this.messages.forEach(message => {
+                messagesArray.push(new FormGroup({
+                    botMessage: new FormControl(message.response),
+                    userMessage: new FormControl(message.utterance),
+                }));
+            });
+        }
+
+
+    }
