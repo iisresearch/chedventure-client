8593d88cb64e229a32cae7ab204163bc4cec5a0b
Kiril <kirilbuga@yahoo.com>
2024-08-16 16:31:35 +0200

    Simplified chatbot-preview using chatbot_url for consistency

--- src/app/features/game-designer/tab-chatbot-preview/chatbot-preview/chatbot-preview.component.ts
+++ src/app/features/game-designer/tab-chatbot-preview/chatbot-preview/chatbot-preview.component.ts
@@ -1,4 +1,4 @@
-import {Component, ElementRef, Input, OnInit, ViewChild} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {Character, Game} from "../../../../core/models/game";
 import {chatbotURL, GameService} from "../../../../core/game.service";
 
@@ -9,8 +9,6 @@ import {chatbotURL, GameService} from "../../../../core/game.service";
 })
 export class ChatbotPreviewComponent implements OnInit {
     @Input() game!: Game;
-
-    @ViewChild('chatbotFrame', {static: false}) chatbotFrame!: ElementRef;
 
     isChatbotRunning: boolean = false;
     
@@ -38,36 +36,11 @@ export class ChatbotPreviewComponent implements OnInit {
     // Initialize chatbot iframe and widget
     private initializeChatbot() {
         this.selectedCharacter = this.characters[0];
-        this.setIframeForChatbot(this.selectedCharacter.id);
-        this.mountChainlitWidget(this.selectedCharacter.id);
     }
     // Check Chatbot server status
     private checkChatbotStatus() {
         this.gameService.checkChatbotStatus().subscribe((status) => {
             this.isChatbotRunning = status;
         });
-    }
-
-    setIframeForChatbot(agentId: number) {
-        // Check if Chatbot is running
-        const url = `${chatbotURL}?character_id=${agentId}`;
-        if(!this.isChatbotRunning) {
-            console.warn('Chatbot server is not available.');
-            return;
-        }
-        if (this.chatbotFrame && this.chatbotFrame.nativeElement)
-            this.chatbotFrame.nativeElement.src = url;
-         else
-            console.error("chatbotFrame is not defined");
-    }
-
-    mountChainlitWidget(agentId: number) {
-        console.log('mountChainlitWidget' in window);
-        if ('mountChainlitWidget' in window) {
-            (window as any).mountChainlitWidget({
-                chainlitServer: `${chatbotURL}/?character_id=${agentId}`,
-                showCot: true, //Chain of Thought
-            });
-        }
     }
 }
