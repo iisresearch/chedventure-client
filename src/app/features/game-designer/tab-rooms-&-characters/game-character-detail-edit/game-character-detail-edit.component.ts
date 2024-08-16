bc4d326c65b7f92f2e2b15d031edbcd6b6b327e3
Kiril <kirilbuga@yahoo.com>
2024-08-16 14:26:54 +0200

    DEFAULT_CHATBOT and chatbotUrl are set automatically

--- src/app/features/game-designer/tab-rooms-&-characters/game-character-detail-edit/game-character-detail-edit.component.ts
+++ src/app/features/game-designer/tab-rooms-&-characters/game-character-detail-edit/game-character-detail-edit.component.ts
@@ -3,6 +3,7 @@ import {Character, Game} from "../../../../core/models/game";
 import {FormControl, FormGroup, Validators} from "@angular/forms";
 import {GameService} from "../../../../core/game.service";
 import {MatSelectChange} from "@angular/material/select";
+import {environment} from "../../../../../environments/environment";
 
 @Component({
   selector: 'app-game-character-detail-edit',
@@ -20,7 +21,7 @@ export class GameCharacterDetailEditComponent implements OnInit {
 
   disabled = true;
 
-  readonly DUMMY_CHATBOT = "https://creator.voiceflow.com/prototype/626abdde3e2ab5c39626f392";
+  DEFAULT_CHATBOT: string = environment.chatbotURL+"/?character_id=-1"; // = environment.chatbotURL+"/?character_id="; // "https://creator.voiceflow.com/prototype/626abdde3e2ab5c39626f392";
 
   characterForm: FormGroup = new FormGroup({
     // name: new FormControl("", [Validators.required]),
@@ -32,7 +33,7 @@ export class GameCharacterDetailEditComponent implements OnInit {
     // utterance: new FormControl(""),
     // response: new FormControl(""),
     //
-    // useDummyChatbot: new FormControl(true),
+    // useDefaultChatbot: new FormControl(true),
     // chatbotUrl: new FormControl("", [Validators.required]),
   })
 
@@ -51,7 +52,7 @@ export class GameCharacterDetailEditComponent implements OnInit {
         description: "",
         title: "",
         history: 0,
-        chatbotUrl: this.DUMMY_CHATBOT,
+        chatbotUrl: this.DEFAULT_CHATBOT,
         contexts: [],
       }
       this.character = ch;
@@ -60,9 +61,10 @@ export class GameCharacterDetailEditComponent implements OnInit {
   }
 
   setupForm() {
+    this.DEFAULT_CHATBOT = environment.chatbotURL+"/?character_id="+this.character?.id;
     let chatbotIsDefault = true;
-    let chatbotURl = this.DUMMY_CHATBOT;
-    if (this.character.chatbotUrl !== this.DUMMY_CHATBOT) {
+    let chatbotURl = this.DEFAULT_CHATBOT;
+    if (this.character.chatbotUrl !== this.DEFAULT_CHATBOT) {
       chatbotIsDefault = false;
       chatbotURl = this.character.chatbotUrl
     }
@@ -72,7 +74,7 @@ export class GameCharacterDetailEditComponent implements OnInit {
       description: new FormControl(this.character.description),
       title: new FormControl(this.character.title),
       history: new FormControl(this.character.history, [Validators.required]),
-      useDummyChatbot: new FormControl(chatbotIsDefault),
+      useDefaultChatbot: new FormControl(chatbotIsDefault),
       chatbotUrl: new FormControl(chatbotURl, [Validators.required]),
     })
   }
@@ -81,14 +83,14 @@ export class GameCharacterDetailEditComponent implements OnInit {
   get description() { return this.characterForm.get('description') }
   get title() { return this.characterForm.get('title') }
   get history() { return this.characterForm.get('history') }
-  get useDummyChatbot() { return this.characterForm.get('useDummyChatbot') }
+  get useDefaultChatbot() { return this.characterForm.get('useDefaultChatbot') }
   get chatbotUrl() { return this.characterForm.get('chatbotUrl') }
 
-  onChangeUseDummyChatbot() {
-    if (this.useDummyChatbot?.value === true) {
-      this.characterForm.controls['chatbotUrl'].patchValue(this.DUMMY_CHATBOT);
+  onChangeUseDefaultChatbot() {
+    if (this.useDefaultChatbot?.value === true) {
+      this.characterForm.controls['chatbotUrl'].patchValue(this.DEFAULT_CHATBOT);
     } else {
-      if (this.chatbotUrl?.value === this.DUMMY_CHATBOT) {
+      if (this.chatbotUrl?.value === this.DEFAULT_CHATBOT) {
         this.characterForm.controls['chatbotUrl'].patchValue("");
       }
     }
@@ -109,17 +111,26 @@ export class GameCharacterDetailEditComponent implements OnInit {
 
       // If a new character is being created, send POST Request
       if (this.createNewCharacter) {
-        this.gameService
-          .createCharacter(this.game.id, characterToSend)
+        this.gameService.createCharacter(this.game.id, characterToSend)
           .subscribe(character => {
-            this.character = character;
-            this.updatedCharacter.emit(this.character);
+            // Update ChatbotUrl to include character id if default chatbot is on
+            if (this.useDefaultChatbot?.value === true) {
+              if (!character.id) throw new Error("Character ID is not defined");
+              character.chatbotUrl = environment.chatbotURL + "/?character_id=" + character.id;
+              this.gameService.updateCharacter(character.id, character)
+                  .subscribe(updatedCharacter => {
+                    this.character = updatedCharacter;
+                    this.updatedCharacter.emit(this.character);
+                  });
+            } else {
+              this.character = character;
+              this.updatedCharacter.emit(this.character);
+            }
           })
 
       } else {
         // Send PUT request to update character to API
-        this.gameService
-          .updateCharacter(this.character.id, characterToSend)
+        this.gameService.updateCharacter(this.character.id, characterToSend)
           .subscribe(character => {
             this.character = character;
             this.updatedCharacter.emit(this.character);
@@ -132,8 +143,7 @@ export class GameCharacterDetailEditComponent implements OnInit {
   deleteCharacter() {
     if (this.character?.id && !this.createNewCharacter) {
       if(confirm("Are you sure you want to delete this character?")) {
-        this.gameService
-          .deleteCharacter(this.character.id)
+        this.gameService.deleteCharacter(this.character.id)
           .subscribe(result => {
             this.deletedCharacter.emit(this.character);
           })
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   apiURL: 'https://chedventure-server.herokuapp.com',
-  urlClient: 'https://chedventure.herokuapp.com'
+  urlClient: 'https://chedventure.herokuapp.com',
+  chatbotUrl: 'http://localhost:8000'
 };
