0d05fb95713f42a54eec46498c409f5ea3908fb7
Kiril <kirilbuga@yahoo.com>
2024-08-27 14:05:01 +0200

    env adjustment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   apiURL: 'https://chedventure-server.herokuapp.com',
   urlClient: 'https://chedventure.herokuapp.com',
-  chatbotUrl: 'http://localhost:8000'
+  chatbotUrl: 'https://chedbot-9bacad87e21b.herokuapp.com'
 };
